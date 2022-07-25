169f7c1bcbeff2f6504c5462df8e4fe5a10d8853
João Guilherme Silva Cabral <46795337+jooguilhermesc@users.noreply.github.com>
2022-07-25 11:27:26 -0300

    correção na transformação para lançar dados na tabela do banco da Azure
    features implementadas:
    
    - código que gera os arquivos csv move os antigos arquivos para a pasta "old_csv" dentro do diretório de ./log
    - insere campos "etl_versao", "etl_dt_inicio", "etl_dt_fim", "dsc_unidade"
    - insere campo "dsc_periodo" com periodicidades M-1
    
    correções:
    
    - omite o erro de lançar cn para senai e cetiqt sesi
    
    implementações pendentes:
    
    - refatorar código para atualizar automaticamente o período

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ from pandas import json_normalize
 from datetime import date
 import pandas as pd
 import requests
+import shutil
 import json
+import os
+
+source = './csv'
+for diretorio, subpastas, arquivos in os.walk(source):
+    for arquivo in arquivos:
+        file_name_in_source = os.path.join(os.path.realpath(diretorio), arquivo)
+        file_name_in_destination = file_name_in_source.replace("\csv","\log\old_csv")
+        shutil.move(file_name_in_source,file_name_in_destination)
 
 data_consulta = date.today()
 url = "https://sorsdn.sistemaindustria.com.br/api/Transparencia"
@@ -10,12 +19,13 @@ payload = ""
 
 uf_list = ["cn","ct","al","ba","se","pe","pb","rn","ce","pi","ma","ac","ap","am","pa","rr","ro","to","ms","mt","go","df","dn","rj","sp","es","mg","rs","pr","sc"]
 entidades = ["sesi","senai"]
+ano = "2022"
 
 for entidade in entidades:
     for uf in uf_list:
         querystring = {"entidade":entidade,
                     "regional":uf,
-                    "ano":"2022"}
+                    "ano":ano}
         response = requests.request("GET", url, data=payload, params=querystring)
 
         if len(response.text) > 962:
@@ -30,6 +40,12 @@ for entidade in entidades:
             
             df = pd.concat([df_receitas,df_despesas,df_despesas_finalidade])
             df["Uf"] = uf.upper()
+            df["nr_ano"] = ano
+            df["etl_versao"] = 2
+            df["etl_dt_inicio"] = "1900-01-01 00:00:00.0000000"
+            df["etl_dt_fim"] = "2200-01-01 00:00:00.0000000"
+            df["dsc_periodo"] = "Jan-Jun"
+            df["dsc_unidade"] = entidade.upper()
             path = f'csv/df_{uf}_{entidade}_{data_consulta}.csv'
             df.to_csv(path, index=False)
             
@@ -37,5 +53,6 @@ for entidade in entidades:
                 arquivo_log.write(f"Status: Sucesso, Entidade: {entidade}, UF: {uf}, Data Consulta: {data_consulta}, Caminho Arquivo: {path}\n")
         
         else:
-            with open(f"log/arquivo_log_{data_consulta}.txt", "a", encoding="utf-8") as arquivo_log:
-                arquivo_log.write(f"Status: Falha, Entidade: {entidade}, UF: {uf}, Data Consulta: {data_consulta}\n")
+            if not (entidade == "senai" and uf == "cn") or not (entidade == "sesi" and uf == "ct"):
+                with open(f"log/arquivo_log_{data_consulta}.txt", "a", encoding="utf-8") as arquivo_log:
+                    arquivo_log.write(f"Status: Falha, Entidade: {entidade}, UF: {uf}, Data Consulta: {data_consulta}\n")
